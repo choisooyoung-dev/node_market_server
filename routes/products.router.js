45838af8894d0e49e2296a4ab6fd2defc0e58732
choisooyoung-dev <sooyoung.choi.dev@gmail.com>
2023-11-05 13:12:42 +0900

    refact: Move variable

--- routes/products.router.js
+++ routes/products.router.js
@@ -17,30 +17,46 @@ router.get("/products", async (req, res) => {
 
 // 상품 상세 조회
 router.get("/products/:productId", async (req, res) => {
+  const { productId } = req.params;
+  // console.log(productId);
+
   try {
-    const { productId } = req.params;
-    console.log(productId);
     const allProducts = await products.find({});
-    // const detail = await products.findOne({ productId });
-    const detail = allProducts.filter(
-      (product) => String(product._id) === productId
-    );
-    console.log(detail);
+
+    // console.log(allProducts);
+
+    // 파라미터로 받은 productid 값과 데이터 고유의 아이디(_id) 값이 일치하는 상품만 걸러내기
+    // 파라미터로 받은 product._id 타입 -> Object => String으로 형변환 시켜서 일치하는지 확인하기
+    // 데이터 안에 존재하는 productId 타입 -> String
+    const detail = allProducts.filter((product) => {
+      // console.log(typeof product._id); -> Object
+      // console.log(typeof productId); -> String
+      // 파라미터로 받은 product._id 타입 -> Object => String으로 형변환 시켜서 일치하는지 확인하기
+      String(product._id) === productId;
+    });
+
+    // 파라미터로 받은 productId 값과 일치하는 데이터의 아이디 값이 없으면
+    if (detail.length < 1) {
+      return res.json({ message: "해당 상품은 존재하지 않습니다." });
+    }
+
+    // 존재하면 해당 상품 보여주기
     return res.status(200).send(detail);
   } catch (error) {
-    return res.json({ fail: "fail" });
+    return res.json({ message: "데이터 형식이 올바르지 않습니다." });
   }
 });
 
 // 상품 등록
 router.post("/products", (req, res) => {
+  const { title, content, author, pwd, status } = req.body;
   try {
     products.create({
-      title: req.body.title,
-      content: req.body.content,
-      author: req.body.author,
-      pwd: req.body.pwd,
-      status: req.body.status,
+      title,
+      content,
+      author,
+      pwd,
+      status,
     });
     return res.status(200).json({ message: "판매 상품을 등록하였습니다." });
   } catch (error) {
@@ -50,30 +66,29 @@ router.post("/products", (req, res) => {
 
 // 상품 수정
 router.put("/products/:productId", async (req, res) => {
+  const { title, content, author, pwd, status } = req.body;
+  const { productId } = req.params;
+  const modifiedProduct = {
+    title,
+    content,
+    author,
+    pwd,
+    status,
+  };
   try {
-    const { title, content, author, pwd, status } = req.body;
-    const { productId } = req.params;
     const allProducts = await products.find({});
     // const detail = await products.findOne({ productId });
     const detail = allProducts.filter(
       (product) => String(product._id) === productId
     );
-    console.log(detail[0]);
-
-    const modifiedProduct = {
-      title,
-      content,
-      author,
-      pwd,
-      status,
-    };
+    // console.log(detail[0]);
 
     await detail[0].updateOne(modifiedProduct);
 
-    return res.json({ message: "modify!" });
+    return res.json({ message: "상품이 수정되었습니다." });
   } catch (error) {
     console.log(req.params);
-    return res.json({ fail: "fail" });
+    return res.json({ message: "해당 상품이 존재하지 않습니다." });
   }
 });
 
